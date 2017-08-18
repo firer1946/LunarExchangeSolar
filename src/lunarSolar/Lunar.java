7009eb59aa6ad2b686ce1639e7b9453a39b1a84e
wangliming <wangliming@cditv.cn>
2017-08-18 16:43:27 +0800

    first

---
+++ src/lunarSolar/Lunar.java
@@ -0,0 +1,14 @@
+package lunarSolar;
+
+/**
+ * Created by firer on 2017/8/18.
+ */
+public class Lunar {
+        public int lunarYear;
+        public int lunarMonth;
+        public int lunarDay;
+        public boolean isleap;
+        public String toString(){
+            return lunarYear+"-"+lunarMonth+"-"+lunarDay;
+        }
+    }
---
+++ src/lunarSolar/Solar.java
@@ -0,0 +1,13 @@
+package lunarSolar;
+
+/**
+ * Created by firer on 2017/8/18.
+ */
+public class Solar {
+        public int solarYear;
+        public int solarMonth;
+        public int solarDay;
+        public String toString(){
+            return solarYear+"-"+solarMonth+"-"+solarDay;
+        }
+}
